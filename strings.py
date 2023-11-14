2027a1fa084e4c4148d0b5d8f76c323ea2be69d5
Hostnomics <hostnomics@gmail.com>
2023-11-13 23:53:59 -0500

    Added common string methods to strings.py

--- strings.py
+++ strings.py
@@ -18,3 +18,32 @@ print('My name is {name} and I am {age}'.format(name=name, age=age))
 # similar to javascript `` but instead we use f string
 print(f'Hello for third time, my name is {name} and I am {age}')
 
+
+# String Methods
+
+s = 'hello world'
+
+# Capitalize first letter of string
+print(s.capitalize())
+
+# All uppercase
+print(s.upper())
+
+# All Lowercase
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get string length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count occurence of something
+sub = 'h'
+print(s.count(sub))
+
+# Resume (15:25): https://youtu.be/JJmcL1N2KQs?si=aUPkVIf6_dvZeiHd&t=925
+
