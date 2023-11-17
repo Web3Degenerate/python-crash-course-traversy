12919813b65fdfa20022290e095f24c0fe3cab12
web3degenerate <hostnomics@gmail.com>
2023-11-16 23:01:01 -0700

    Added for in loop to loops.py

--- loops.py
+++ loops.py
@@ -1,4 +1,11 @@
 # A for loop is used for iterating over a sequence 
-# (that is either a list, a tuple, a dictionary, a set or a string)
+# (that is either a list, a tuple, a dictionary, a set or a string) (or a custom condition)
+
+
+people = ['John', 'Paul', 'Sara']
+
+# Simple for loop
+for person in people:
+    print(f'Current Person is {person}')
 
 # While loops execute a set of statements as long as a condition is true.
