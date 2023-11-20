2698c4231c58e25c09b4555d2309f8ff95469ad1
hostnomics <hostnomics@gmail.com>
2023-11-19 20:45:38 -0500

    Imported camelcase module to modules.py

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Python Crash Course from [Travery Media YouTube Tutorial](https://www.youtube.com/watch?v=JJmcL1N2KQs&t=1111s)
 
-
-
 1. Download Python from [Python.org](https://www.python.org/)
 
-2. 
+2.
 
+## _Resume at [59:09](https://youtu.be/JJmcL1N2KQs?si=8Wzh4dnsltXJ-k22&t=3549)_ Start **loops.py**.
 
-## _Resume at [59:09](https://youtu.be/JJmcL1N2KQs?si=8Wzh4dnsltXJ-k22&t=3549)_ Start **loops.py**. 
+3.  At (1:09:00) Covers Python package manager 'pip': https://youtu.be/JJmcL1N2KQs?si=HyOOaLhCfsHJx34W&t=4140
+
+- `pip install camelcase`
+- _similar to running `npm i whatever --global`_
+
+4.  Look into his Django udemy course.
+
+5.  See all modules installed with `pip freeze` (1:11:00)
+
+- here, returned `camelcase==0.2`
--- modules.py
+++ modules.py
@@ -5,9 +5,43 @@
 # (including Django) as well as custom modules. 
 
 # (1:05:46) Import the dateTime module: 
+# Here the date object is IN datetime:
 
+# import datetime
+# today = datetime.date.today() # See docs for all the properties available.
+# print(today)
+
+# (1:06:44) import JUST date from datetime, instead of importing all of datetime
+# Core modules (datetime and time)
 import datetime
-
-today = datetime.date.today()
-
+from datetime import date
+today = date.today() 
 print(today)
+
+
+# Import Time
+# import time
+# timestamp = time.time()
+
+import time
+from time import time
+timestamp = time()
+print(timestamp)
+
+
+# At (1:09:00) Covers Python package manager 'pip': https://youtu.be/JJmcL1N2KQs?si=HyOOaLhCfsHJx34W&t=4140
+# install 'camelcase' from pip with
+# 'pip install camelcase' - (he used 'pip3 install camelcase')
+
+# Pip module (camelcase)
+# import camelcase
+
+from camelcase import CamelCase
+
+c = CamelCase()
+
+print(c.hump('hello there world'))
+
+
+# At (1:13:05) Example of Custom Module to be imported: https://youtu.be/JJmcL1N2KQs?si=C32j1fOkWALZYydn&t=4385
+
